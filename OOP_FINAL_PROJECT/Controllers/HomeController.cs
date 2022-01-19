a84b55790c12a8baa278e0a1c3b3022a6cd75389
tanko <tanko.tudor.20@stud.umfst.ro>
2022-01-19 17:06:33 +0200

    testing git

--- OOP_FINAL_PROJECT/Controllers/HomeController.cs
+++ OOP_FINAL_PROJECT/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace OOP_FINAL_PROJECT.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description pg.";
 
             return View();
         }
